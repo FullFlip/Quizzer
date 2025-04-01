052e2d3a9cbf7dbc1856cba2a927be1fdfb87988
JarnoRyhanen <jarno.ryhanen02@gmail.com>
2025-04-01 14:55:22 +0300

    Initial commit

---
+++ src/main/java/com/haagahelia/quizzer/web/quizcontroller.java
@@ -0,0 +1,17 @@
+package com.haagahelia.quizzer.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class quizcontroller {
+    
+    @GetMapping("/test")
+    @ResponseBody
+    public String testEndpoint() {
+        return "This is a test endpoint!";
+    }
+}
