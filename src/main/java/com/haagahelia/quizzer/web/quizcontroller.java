9adf0119a455549d4695de8fbc1fdb9b58845bc0
Valheri <valtteri.vuokila@gmail.com>
2025-04-15 14:35:03 +0300

    Controller endpoints for adding editing and deleting a question and a quiz + getQuiz method to Question

--- src/main/java/com/haagahelia/quizzer/web/quizcontroller.java
+++ src/main/java/com/haagahelia/quizzer/web/quizcontroller.java
@@ -5,17 +5,22 @@ import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.haagahelia.quizzer.model.Question;
 import com.haagahelia.quizzer.model.Quiz;
 import com.haagahelia.quizzer.model.Teacher;
+import com.haagahelia.quizzer.repositories.QuestionRepository;
 import com.haagahelia.quizzer.repositories.QuizRepository;
 import com.haagahelia.quizzer.services.QuizService;
 import com.haagahelia.quizzer.services.TeacherService;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class quizcontroller {
@@ -29,13 +34,16 @@ public class quizcontroller {
     @Autowired
     private QuizRepository quizRepository;
 
+    @Autowired
+    private QuestionRepository questionRepository;
+
     @GetMapping("/test")
     @ResponseBody
     public Stream<Teacher> testEndpoint() {
         return teacherService.findOnlyTeacherData();
     }
 
-    @CrossOrigin(origins = "http://localhost:5174")
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/quizzes/{id}")
     public List<Quiz> getAllQuizzesByTeacher(@PathVariable Long id) {
 
@@ -50,6 +58,67 @@ public class quizcontroller {
 
         List<Quiz> listOfQuizzes = quizService.getAllQuizzesByTeacher(teacherId.getTeacherId());
         return listOfQuizzes;
+    }
+
+    @CrossOrigin(origins = "http://localhost:5173")
+    @PostMapping("/quizzes")
+    public Quiz addQuiz(@RequestBody Quiz quiz) {
+        if (quiz == null) {
+            throw new IllegalArgumentException("Quiz data is not provided or is invalid.");
+        }
+        return quizRepository.save(quiz);
+    }
+
+    @CrossOrigin(origins = "http://localhost:5173")
+    @PutMapping("/quizzes/{id}")
+    public Quiz editQuiz(@PathVariable Long id, @RequestBody Quiz updatedQuiz) {
+        Quiz existingQuiz = quizRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Quiz with id " + id + " could not be found"));
+        existingQuiz.setTitle(updatedQuiz.getTitle());
+        existingQuiz.setDescription(updatedQuiz.getDescription());
+        existingQuiz.setCourseCode(updatedQuiz.getCourseCode());
+        existingQuiz.setPublishedStatus(updatedQuiz.isPublishedStatus());
+        existingQuiz.setPublishedDate(updatedQuiz.getPublishedDate());
+        return quizRepository.save(existingQuiz);
+    }
+
+    @CrossOrigin(origins = "http://localhost:5173")
+    @DeleteMapping("/quizzes/{id}")
+    public void deleteQuiz(@PathVariable Long id) {
+        Quiz quiz = quizRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Quiz with id " + id + " could not be found"));
+        quizRepository.delete(quiz);
+    }
+
+    // Enpoints for Question
+
+    @CrossOrigin(origins = "http://localhost:5173")
+    @PostMapping("/questions")
+    public Question addQuestion(@RequestBody Question question) {
+        if (question == null) {
+            throw new IllegalArgumentException("Question data is not provided or is invalid.");
+        }
+        return questionRepository.save(question);
+    }
+
+    @CrossOrigin(origins = "http://localhost:5173")
+    @PutMapping("/questions/{id}")
+    public Question editQuestion(@PathVariable Long id, @RequestBody Question updatedQuestion) {
+        Question existingQuestion = questionRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Question with id " + id + " could not be found"));
+        existingQuestion.setTitle(updatedQuestion.getTitle());
+        existingQuestion.setDifficulty(updatedQuestion.getDifficulty());
+        existingQuestion.setChoices(updatedQuestion.getChoices());
+        existingQuestion.setQuiz(updatedQuestion.getQuiz());
+        return questionRepository.save(existingQuestion);
+    }
+
+    @CrossOrigin(origins = "http://localhost:5173")
+    @DeleteMapping("/questions/{id}")
+    public void deleteQuestion(@PathVariable Long id) {
+        Question question = questionRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Question with id " + id + " could not be found"));
+        questionRepository.delete(question);
     }
 
     @GetMapping("/matti")
